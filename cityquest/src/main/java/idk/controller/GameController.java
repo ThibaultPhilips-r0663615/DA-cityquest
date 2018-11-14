0cc7562cce5fcba8879243ae5e81b6f705e1e2d0
Wouter Engelen <wouter.engelen@student.ucll.be>
2018-11-14 14:17:47 +0100

    Implemented REST API

--- cityquest/src/main/java/idk/controller/GameController.java
+++ cityquest/src/main/java/idk/controller/GameController.java
@@ -2,35 +2,57 @@ package idk.controller;
 
 import idk.database.GameRepository;
 import idk.model.Game;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import static idk.model.Game.GameBuilder.aGame;
-import static idk.model.Question.QuestionBuilder.aQuestion;
-
+@RequestMapping("/games")
 @RestController
 public class GameController {
 
     private GameRepository gameRepository;
 
-    // Geen autowired recuired => gebeurt automatisch wanneer je parameeter meegeeft aan constructor
+    // Geen autowired required => gebeurt automatisch wanneer je parameter meegeeft aan constructor
     // Bean wordt aangemaakt door interface die CrudRepository implementeert.
     public GameController(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
     }
 
-    @RequestMapping("/getGames")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Game> getGames() {
         return StreamSupport
                 .stream(gameRepository.findAll().spliterator(), false)
-                .map(game -> {game.setQuestions(new ArrayList<>()); return game;})
+                .peek(game -> game.setQuestions(new ArrayList<>()))
                 .collect(Collectors.toList());
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Game addGame(@RequestBody Game game){
+        return gameRepository.save(game);
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Game getGame(@PathVariable("id") UUID id){
+        return gameRepository.findById(id).orElseThrow(GameNotFoundException::new);
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
+    public Game updateGame(@PathVariable("id") UUID id, @RequestBody Game game){
+        if(!id.equals(game.getId())) throw new MethodNotAllowedException();
+        return gameRepository.save(game);
+    }
+
+    @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "game not found")
+    private class GameNotFoundException extends RuntimeException {
+    }
+
+    @ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
+    private class MethodNotAllowedException extends RuntimeException {
+    }
+
 }
